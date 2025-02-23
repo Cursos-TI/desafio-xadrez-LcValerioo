3925dab10aa237b16bc99f12888e6fb95cab80d2
LcValerioo <126265243+LcValerioo@users.noreply.github.com>
2025-02-23 04:29:09 +0000

    adicionando recursividade (mestre)

--- xadrez.c
+++ xadrez.c
@@ -1,47 +1,66 @@
 #include <stdio.h>
+//procedimento do movimento da Rainha
+void rainha(int i)
+{
+    if(i > 0)
+    {
+        printf("Esquerda.\n");//Impressão do movimento
+        rainha(i - 1);
+    }
+}
+//procedimento do movimento da Torre
+void torre(int i)
+{
+    if(i > 0)
+    {
+        printf("Direita.\n");//Impressão do movimento
+        torre(i - 1);
+    }
+}
+//procedimento do movimento do Bispo
+void bispo(int i)
+{
+    if(i > 0)
+    {
+        int j = i;
+        printf("Cima.\n"); //Impressão do movimento
+        if(j == i){
+            printf("Direita.\n"); //Impressão do movimento
+        }
+        bispo(i - 1);
+        
+    }
+
+}
 
 int main()
 {
     //Declarando as Variaveis.
-   int i, j;
+   int i = 8, j = 5, z = 5;
    
-   //Laço de movimento (for) do Bispo
-   for(i = 1; i <= 5; i++)
-   {
-       printf("O Bispo moveu para Casa Superior Direita.\n"); //Impressão do movimento
-   }
-   printf("\n\n");
+   //Chamando os procedimentos do movimento das peças
+   printf("Movendo o Bispo...\n");
+   bispo(z);
+   printf("\n");//Pulando linha entre as peças.
    
-   //Laço de movimento (while) da Torre
-   i = 1; //A partir desse momento a variavel passa a ter valor 1.
-   while(i <= 5)
-   {
-       printf("A Torre moveu para a Direita.\n");//Impressão do movimento
-       i++; //incrementando a variavel para sair do loop
-   }
+   printf("Movendo a Torre...\n");
+   torre(j);
+   printf("\n");//Pulando linha entre as peças.
    
-   printf("\n\n"); //Pulando linha entre as peças.
+   printf("Movendo a Rainha...\n");
+   rainha(i);
+   printf("\n"); //Pulando linha entre as peças.
    
-   //Laço de movimento (do while) da Rainha
-   j = 1; //A partir desse momento a variavel passa a ter valor 1.
-   do 
-   {
-       printf("A Rainha se moveu para a Esquerda.\n");//Impressão do movimento
-       j++;
-   }while(j <= 8);
-   
-   printf("\n\n"); //Pulando linha entre as peças.
-   
-    //Laço de movimento (for, while) do Cavalo (loop aninhados)
-    for(i = 1; i <= 2; i++)
+    //Laço de movimento (for) do Cavalo (loop complexo)
+    printf("Movendo o Cavalo...\n");
+    for(i = 1; i <= 3; i++)
     {
-        j = i;
-        printf("O Cavalo se Moveu para a Esquerda.\n");//Impressão do movimento
-        while(j == 2)
+        if(i == 3)
         {
-            printf("O Cavalo se Moveu para a Baixo.\n");//Impressão do movimento
-            j++; //incrementando a variavel para sair do loop
+            printf("Direita.\n");//Impressão do movimento
+            break;
         }
+        printf("Cima.\n");//Impressão do movimento
          
     }
     return 0;
